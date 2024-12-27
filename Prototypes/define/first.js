dd72230a9e0ffba3fb957647dc25b1fe9602366a
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2024-12-26 23:33:31 -0300

    Update first.js

--- Prototypes/define/first.js
+++ Prototypes/define/first.js
@@ -1,28 +1,28 @@
 function Item(name, price, stock) {
-    Object.defineProperty(this,'stock',{
-        enumerable:true,
-        value:stock,
-        writable:false,
-        configurable:false,
+    Object.defineProperty(this, 'stock', {
+        enumerable: true,
+        value: stock,
+        writable: false,
+        configurable: false,
     });
-    Object.defineProperties(this,{
+    Object.defineProperties(this, {
         name: {
-            enumerable:true,
-            value:name,
-            writable:true,
-            configurable:false,
-            
+            enumerable: true,
+            value: name,
+            writable: true,
+            configurable: false,
+
         },
-        price:{
-            enumerable:true,
-            value:price,
-            writable:false,
-            configurable:false,
+        price: {
+            enumerable: true,
+            value: price,
+            writable: false,
+            configurable: false,
         },
     })
 }
 
-const p1 = new Item('Soap',3 ,28);
+const p1 = new Item('Soap', 3, 28);
 
 console.log(p1)
 p1.name = 'Scarf'
